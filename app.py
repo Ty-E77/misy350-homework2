38fc7b6a868f320149aa6caff22579f7ded46828
TyShawn <tyshawn@udel.edu>
2026-03-04 13:04:41 -0500

    created the Place order tab outline

--- app.py
+++ app.py
@@ -3,16 +3,12 @@ import streamlit as st
 import json
 from pathlib import Path
 
-json_file = Path("inventory.json")
+json_file1 = Path("inventory.json")
 
-if json_file.exists():
-    with open(json_file, "r") as f:
-        inventory = json.load(f)
-else:
-    # Default data if file doesn't exist
-    inventory = [] 
+json_file2 = Path("orders.json")
 
-next_order_id_number = 6
+
+next_order_id_number = 3
 
 # For saving after changing information
 #with open(json_file, "w") as f:
@@ -28,6 +24,13 @@ st.divider()
 
 tab1, tab2, tab3, tab4 = st.tabs(["Place Order", "View Inventory", "Restock", "Manage Orders"])
 
+if json_file1.exists():
+    with open(json_file1, "r") as f:
+        inventory = json.load(f)
+
+if json_file2.exists():
+    with open(json_file2, "r") as f:
+        orders = json.load(f)
 with tab1:
     st.subheader("Place Order")
     with st.container(border = True):
@@ -36,54 +39,65 @@ with tab1:
         # col1, col2 = st.columns(2) this will give you 50/50 
         # col1, col2, col3 = st.columns([1,6,1]) # changes spacing 
 
-        with col1:
-            with st.container(border = True):
-                st.markdown("##### Order Details")
-                title = st.text_input("Item Name", placeholder = "Expresso", help = "Enter the item name.")
-                price = st.number_input("Price")
-                stock = st.number_input("Stock")
+    with col1:
+        with st.container(border = True):
+            st.markdown("##### Order Details")
+            place_order = []
+
+            selected_item = st.selectbox("Select Item", options = inventory,
+                                           format_func = lambda x: f"{x['name']}")
+            if selected_item:
+                with st.expander("Order Details", expanded = True):
+                    st.markdown(f"### Item: {selected_item["name"]}")
+                    st.markdown(f"Unit Price: {selected_item["price"]}")
+                    quantity_orederd = int(st.number_input("Enter the order quantity", 
+                                                           min_value = 0, 
+                                                           key = f"quantity_ordered_{selected_item["id"]}"))
                 
-                #assignment_type = st.text_input("Assignment Type") # wrong bc we know the tyoes
-                assignment_type = st.radio("Type", ["Homework", "Lab"], horizontal=True)
-                st.caption("Homework Type")
-                assignment_type2 = st.selectbox("Type", ["Select an option","Homework", "Lab", "Other"])
-                if assignment_type2 == "Other":
-                    assignment_type2 = st.text_input("Type", placeholder = "Enter the assignment Type")
+                    total_price = quantity_orederd * selected_item["price"]
 
-
-        with col2:
-            st.markdown("**Due Date Selection**")
-            due_date = st.date_input("Due Date")
+                    st.markdown(f"Total Price: {total_price}")
         
-        btn_save = st.button("Save", use_container_width = True, disabled = False) # width = "content"(len of content), width = "stretch" 
+
+            
+            
+    
+            #enter_quantity =
+             
+            #show_total_price =  
+
+            #edit_name = st.text_input("Item Name", key = f"edit_name_{place_order_edit["id"]}", value = place_order_edit["name"])
+
         
-        if btn_save:
-            if not title:
-                st.warning("Title needs to be provided!")
-            else:
-                with st.spinner("Assignment is being recorded..."):
-                    time.sleep(5)
+        # btn_save = st.button("Save", use_container_width = True, disabled = False) # width = "content"(len of content), width = "stretch" 
+        
+        # if btn_save:
+        #     if not title:
+        #         st.warning("Title needs to be provided!")
+        #     else:
+        #         with st.spinner("Assignment is being recorded..."):
+        #             time.sleep(5)
 
-                    new_assignment_id = "HW" + str(next_order_id_number)
-                    next_order_id_number += 1
+        #             new_assignment_id = "HW" + str(next_order_id_number)
+        #             next_order_id_number += 1
 
-                    assignments.append(
-                        {
-                            "id" : new_assignment_id,
-                            "title" : title,
-                            "description" : description,
-                            "points" : points,
-                            "type" : assignment_type
+        #             assignments.append(
+        #                 {
+        #                     "id" : new_assignment_id,
+        #                     "title" : title,
+        #                     "description" : description,
+        #                     "points" : points,
+        #                     "type" : assignment_type
 
-                        }
-                    )
+        #                 }
+        #             )
 
-                    # record into json file 
-                    with json_path.open("w", encoding = "utf-8") as f:
-                        json.dump(assignments, f)
+        #             # record into json file 
+        #             with json_path.open("w", encoding = "utf-8") as f:
+        #                 json.dump(assignments, f)
 
 
 
-                    st.success("New Assignment is recorded!")
-                    st.info("This is a new assignment")
-                    st.dataframe(assignments)
+        #             st.success("New Assignment is recorded!")
+        #             st.info("This is a new assignment")
+        #             st.dataframe(assignments)
